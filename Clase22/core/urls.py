3fee863ebbe0ff8e39b8cf195461103f7afd7d6f
Cesar <cesarbaby@gmail.com>
2023-03-15 20:18:49 -0300

    Agregados 4 templates

--- Clase22/core/urls.py
+++ Clase22/core/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import inicio
+from core.views import inicio, agregar, editar, mostrar, eliminar
 
 urlpatterns = [
     path('', inicio, name="index"),
+    path('agregar/', agregar, name="agregar"),
+    path('editar/', editar, name="editar"),
+    path('mostrar/', mostrar, name="mostrar"),
+    path('eliminar/', eliminar, name="eliminar"),
 
 ]
--- Clase22/core/views.py
+++ Clase22/core/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, 'core/index.html')
+
+def agregar(request):
+    return render(request, 'core/agregar_curso.html')
+
+def mostrar(request):
+    return render(request, 'core/mostrar_curso.html')
+
+def editar(request):
+    return render(request, 'core/editar_curso.html')
+
+def eliminar(request):
+    return render(request, 'core/eliminar_curso.html')
+
